2f6c1772733945e448ef74d74310475901a9e64f
Russ Allbery <rra@lsst.org>
2023-05-26 11:48:51 -0700

    Add reDoc documentation for proof-of-concept
    Add the schema for the protocol of the proof-of-concept alternate
    implementation, and generate reDoc documentation for it. Link that
    to the tech note.

--- conf.py
+++ conf.py
@@ -7,3 +7,17 @@ https://documenteer.lsst.io/technotes/configuration.html
 """
 
 from documenteer.conf.technote import *  # noqa: F401, F403
+
+extensions.extend(["sphinxcontrib.redoc"])
+redoc = [
+    {
+        "name": "Proof of concept REST API",
+        "page": "poc-schema",
+        "spec": "_static/poc-schema.json",
+        "embed": True,
+        "opts": {"hide-hostname": True},
+    }
+]
+redoc_uri = (
+    "https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js"
+)
